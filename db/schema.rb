b9f3c3e0365fd4aedaea1c1d2aefe74939178a13
Garbage-soft <wake.awakens3@gmail.com>
2022-06-18 22:23:49 +0900

    add usermodel and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_15_094301) do
+ActiveRecord::Schema.define(version: 2022_06_18_125543) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.integer "price"
     t.integer "stock"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
