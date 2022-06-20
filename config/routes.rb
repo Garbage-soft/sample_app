ae068a53fba3b70aa427096566cd292971ee7e39
Garbage-soft <wake.awakens3@gmail.com>
2022-06-20 14:56:14 +0900

    4.4 routes.rbにsessionsリソースの名前付きルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root "products#index"
-  get  '/signup',  to: 'users#new'
-  get '/login', to: 'sessions#new'
+  root   "products#index"
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete 'logout'    to: 'sessions#destroy'
   resources :products
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
