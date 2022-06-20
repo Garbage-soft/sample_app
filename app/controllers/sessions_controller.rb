4b32865abb5b83e792da7f96ed8a2df36b3f01da
Garbage-soft <wake.awakens3@gmail.com>
2022-06-20 15:03:18 +0900

    4.4 sessionsコントローラにcreate, destroyアクション追加（実装は後）

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,14 @@ class SessionsController < ApplicationController
 
   def new
   end
+
+  def create
+    @user = User.find_by(email: params[:session][:email].downcase)
+    # ログインの処理を書く
+  end
+
+  def destroy
+    # ログアウトの処理を書く
+    redirect_to root_url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  delete 'logout'    to: 'sessions#destroy'
+  delete 'logout',   to: 'sessions#destroy'
   resources :products
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
