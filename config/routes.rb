8634825c7e8fe06d622d299899199ffa505cc15a
Garbage-soft <wake.awakens3@gmail.com>
2022-06-20 14:54:25 +0900

    4.3 ログインページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "products#index"
   get  '/signup',  to: 'users#new'
+  get '/login', to: 'sessions#new'
   resources :products
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
