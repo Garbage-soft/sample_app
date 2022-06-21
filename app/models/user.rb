ba074382f27c282dff8a30ed23ca8539790ed8bf
Garbage-soft <wake.awakens3@gmail.com>
2022-06-21 20:42:58 +0900

    4.6 Userモデルにバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  # バリデーション ===============================================================================
+  # 名前 　　　　　検証内容：　存在性、　長さ（最大50文字）
+  validates :name,  presence: true, length: { maximum: 50 }
+
+  # メールアドレスの形式を指定する定数（正規表現）
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  # メールアドレス　検証内容：　存在性、　長さ（最大255文字）、　フォーマット(形式)、　一意性(大文字小文字を区別しない)
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+
+  # パスワード　　　検証内容：　存在性
   validates :password, presence: true
+  # ============================================================================================
 end
