91c3ab2673526a71ccb10d289bc698828e35daaf
Garbage-soft <wake.awakens3@gmail.com>
2022-06-21 20:48:26 +0900

    4.7 Userモデルにインデックス追加（emailに一意性強制）、Userモデルにコールバックメソッド設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_20_061233) do
+ActiveRecord::Schema.define(version: 2022_06_21_114327) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_06_20_061233) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
