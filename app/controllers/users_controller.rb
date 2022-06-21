af17b8600ac3694a7ed23fcb99af9ffef9b67c70
Garbage-soft <wake.awakens3@gmail.com>
2022-06-21 21:38:23 +0900

    4.10 Bootstrap導入

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
   end
@@ -10,10 +14,6 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
-  end
-
-  def show
-    @user = User.find(params[:id])
   end
 
   def edit
